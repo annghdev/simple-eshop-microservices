e1344f1f34adb1c0272e261d8d529ee8d51852c1
John Doe <annghdev@gmail.com>
2026-03-18 17:11:28 +0700

    Add MapScalar API document and setup Aspire

--- src/Aspire.AppHost/AppHost.cs
+++ src/Aspire.AppHost/AppHost.cs
@@ -1,11 +1,52 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.APIGateway>("apigateway");
+// setup dependencies
+//var docker = builder.AddDockerComposeEnvironment("eshop");
 
-builder.AddProject<Projects.Catalog_API>("catalog-api");
+var redis = builder.AddRedis("redis");
 
-builder.AddProject<Projects.Inventory_API>("inventory-api");
+var rabbitMq = builder.AddRabbitMQ("rabbitmq");
 
-builder.AddProject<Projects.Order_API>("order-api");
+var postgres = builder.AddPostgres("postgres").WithPgWeb();
+
+var commonDb = postgres.AddDatabase("commondb");
+var catalogDb = postgres.AddDatabase("catalogdb");
+var inventoryDb = postgres.AddDatabase("inventorydb");
+var orderDb = postgres.AddDatabase("orderdb");
+
+
+//Projects
+var catalog = builder.AddProject<Projects.Catalog_API>("catalog")
+    .WithReference(catalogDb)
+    .WithReference(redis)
+    .WithReference(rabbitMq)
+    .WaitFor(catalogDb)
+    .WaitFor(redis)
+    .WaitFor(rabbitMq);
+
+
+var inventory = builder.AddProject<Projects.Inventory_API>("inventory")
+    .WithReference(rabbitMq)
+    .WithReference(redis)
+    .WithReference(catalogDb)
+    .WaitFor(catalogDb)
+    .WaitFor(redis)
+    .WaitFor(rabbitMq);
+
+var order = builder.AddProject<Projects.Order_API>("order")
+    .WithReference(redis)
+    .WithReference(rabbitMq)
+    .WithReference(catalogDb)
+    .WaitFor(catalogDb)
+    .WaitFor(redis)
+    .WaitFor(rabbitMq);
+
+var gateway = builder.AddProject<Projects.APIGateway>("apigateway")
+    .WithReference(redis)
+    .WithReference(rabbitMq)
+    .WithReference(commonDb)
+    .WaitFor(commonDb)
+    .WaitFor(redis)
+    .WaitFor(rabbitMq);
 
 builder.Build().Run();
